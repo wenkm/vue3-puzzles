dff4f353877b1d2c6436cf7bcf806771950c7533
Kevin <i@mkev.in>
2022-04-29 17:21:30 +0800

    兼容手机

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-    <div id="box" :style="`--size: ${size}px`">
+    <div id="box" :style="`--size: ${size}px; --scale: ${scale}`">
         <div class="action">
             <div class="set">
                 <button @click="number--" :disabled="number <= 3">-</button>
-                <span>{{number}}阶</span>
+                <span>{{ number }}阶</span>
                 <button @click="number++" :disabled="number >= 8">+</button>
             </div>
             <button @click="shuffleHandle" :disabled="!isStart">打乱</button>
             <button @click="startHandle" :disabled="isStart">开始</button>
             <button @click="isStart = false" :disabled="!isStart">停止</button>
-            <timer :start="isStart"></timer>
+            <!-- <button @click="autoHandle" :disabled="!isStart">自动</button> -->
+            
         </div>
-        <transition-group name="puzzle" tag="div" class="container" :style="{width: `${number * 100}px`, height: `${number * 100}px`}">
-            <div class="item" :class="{empty: item === ''}" v-for="item, index in puzzles" :key="item" @mousedown="clickHandle(index, item)" :style="`background-position: ${-100 * (item - 1)}px ${-100 * ~~((item - 1) / number)}px`">
+        <timer :start="isStart"></timer>
+        <transition-group name="puzzle" tag="div" class="container" :style="{width: `${size}px`, height: `${size}px`}">
+            <div class="item" :class="{empty: item === 0}" v-for="(item, index) in puzzles" :key="item" @mousedown="clickHandle(index, item)" @touchstart.prevent="clickHandle(index, item)" :style="`background-position: ${-100 * (item - 1)}px ${-100 * ~~((item - 1) / number)}px`">
                 <span>{{ item }}</span>
             </div>
         </transition-group>
@@ -20,31 +22,34 @@
 </template>
 <script setup>
 import timer from '@/components/timer.vue';
-import {ref, watch} from 'vue';
+import {ref, watch, nextTick} from 'vue';
 const puzzles = ref([]),
     number = ref(3),
     size = ref(0),
+    scale = ref(1),
     isStart = ref(false);
-watch(number, v => {
-    if ('' === v) {
-        return;
-    }
-    size.value = 100 * v;
-    puzzles.value = [];
-    for (let i = 1; i < v * v; i++) {
-        puzzles.value.push(i);
-    }
-    puzzles.value.push('');
-    isStart.value = false;
-}, {
-    immediate: true
-});
+watch(
+    number,
+    v => {
+        size.value = 100 * v;
+        puzzles.value = [];
+        for (let i = 1; i < v * v; i++) {
+            puzzles.value.push(i);
+        }
+        puzzles.value.push(0);
+        scale.value = size.value > window.innerWidth ? window.innerWidth / size.value : 1;
+        isStart.value = false;
+    },
+    {
+        immediate: true,
+    },
+);
 function shuffle(arr) {
     return arr.sort(() => Math.random() - 0.5);
 }
 function shuffleHandle() {
-    puzzles.value = shuffle(puzzles.value.filter(item => item !== ''));
-    puzzles.value.push('');
+    puzzles.value = shuffle(puzzles.value.filter(item => item !== 0));
+    puzzles.value.push(0);
 }
 function clickHandle(index, item) {
     if (!isStart.value) {
@@ -54,18 +59,18 @@ function clickHandle(index, item) {
         rightIndex = index + 1,
         topIndex = index - number.value,
         bottomIndex = index + number.value;
-    if (leftIndex >= 0 && puzzles.value[leftIndex] === '') {
+    if (leftIndex >= 0 && puzzles.value[leftIndex] === 0) {
         puzzles.value[leftIndex] = item;
-        puzzles.value[index] = '';
-    } else if (rightIndex <= Math.pow(number.value, 2) - 1 && puzzles.value[rightIndex] === '') {
+        puzzles.value[index] = 0;
+    } else if (rightIndex <= Math.pow(number.value, 2) - 1 && puzzles.value[rightIndex] === 0) {
         puzzles.value[rightIndex] = item;
-        puzzles.value[index] = '';
-    } else if (topIndex >= 0 && puzzles.value[topIndex] === '') {
+        puzzles.value[index] = 0;
+    } else if (topIndex >= 0 && puzzles.value[topIndex] === 0) {
         puzzles.value[topIndex] = item;
-        puzzles.value[index] = '';
-    } else if (bottomIndex <= Math.pow(number.value, 2) - 1 && puzzles.value[bottomIndex] === '') {
+        puzzles.value[index] = 0;
+    } else if (bottomIndex <= Math.pow(number.value, 2) - 1 && puzzles.value[bottomIndex] === 0) {
         puzzles.value[bottomIndex] = item;
-        puzzles.value[index] = '';
+        puzzles.value[index] = 0;
     }
     if (isSuccess()) {
         timer.isStart = false;
@@ -85,22 +90,37 @@ function startHandle() {
     isStart.value = true;
     shuffleHandle();
 }
+function chunkArr(arr, size) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+        newArr.push(arr.slice(i, i + size));
+    }
+    return newArr;
+}
+
 </script>
 <style lang="less">
+html, body{
+    margin: 0;
+    padding: 0;
+}
 .container {
     display: flex;
     flex-wrap: wrap;
-    perspective: 1200px;
     border: solid 1px #000;
     overflow: hidden;
+    transform: scale(var(--scale));
+    transform-origin: 0 0;
 }
-.action{
+.action {
     display: flex;
-    gap: 10px;
+    column-gap: 10px;
     margin-bottom: 10px;
     align-items: center;
+    flex-wrap: wrap;
+    
 }
-button{
+button {
     background: linear-gradient(30deg, #f64af6, #c2da62);
     border: solid 1px #7a1e7a;
     border-radius: 4px;
@@ -108,14 +128,14 @@ button{
     height: 30px;
     padding: 0 15px;
     user-select: none;
-    &:disabled{
+    &:disabled {
         opacity: 0.5;
     }
 }
-.set{
+.set {
     display: flex;
     align-items: center;
-    span{
+    span {
         margin: 0 10px;
     }
 }
@@ -128,10 +148,10 @@ button{
     background-size: var(--size) var(--size);
     position: relative;
     user-select: none;
-    &:hover{
+    &:hover {
         opacity: 0.5;
     }
-    span{
+    span {
         padding: 5px 10px;
         background: rgba(0, 0, 0, 0.3);
         position: absolute;
@@ -141,10 +161,10 @@ button{
         border-radius: 0 0 10px 0;
         font-size: 12px;
     }
-    &.empty{
+    &.empty {
         background: none;
         box-shadow: none;
-        span{
+        span {
             display: none;
         }
     }
